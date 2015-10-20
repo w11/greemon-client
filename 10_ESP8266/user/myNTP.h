d800b59a5cb559055e8073872aa923d8bdf55d89
David Huber <w11@teamnwf.de>
2015-10-20 16:47:55 +0200

    visual changes

--- 10_ESP8266/user/myNTP.h
+++ 10_ESP8266/user/myNTP.h
@@ -9,7 +9,6 @@
 #include "user_interface.h"
 #include "osapi.h"
 
-
 #define USER_NTP_TIMEZONE +1
 #define USER_NTP_SRV1 "0.europe.pool.ntp.org"
 #define USER_NTP_SRV2 "1.europe.pool.ntp.org"
@@ -17,6 +16,7 @@
 
 #define USER_NTP_POLLING_INTERVAL 10000
 #define USER_NTP_MAX_TRIES 5
+
 static volatile uint8_t user_ntp_tries = 0;
 static volatile uint32_t user_ntp_timestamp = 0;
 static volatile bool user_ntp_valid_time = false;
